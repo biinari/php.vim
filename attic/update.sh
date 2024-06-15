2344c8e44d54046d0b622209ee6ded4271bd0688
Bill Ruddock <bill.ruddock@gmail.com>
2024-06-15 04:30:14 +0100

    Add PHP 8.0 names

--- attic/update.sh
+++ attic/update.sh
@@ -4,10 +4,10 @@ cat - > php.vim
 
 test -e update.out && rm update.out
 
-for version in 5.6 7.0 7.1 7.2 7.3 7.4; do
+for version in 5.6 7.0 7.1 7.2 7.3 7.4 8.0; do
     php$version 10-collect.php 1>>update.out
 done
 
 cat update.out | \
-    php7.4 20-generate.php | \
-    php7.4 30-update.php php.vim
+    php8.0 20-generate.php | \
+    php8.0 30-update.php php.vim
