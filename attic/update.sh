93ce32e13bde44a099a73ac134a39bd0a78780bf
Bill Ruddock <bill.ruddock@gmail.com>
2024-06-15 21:30:17 +0100

    Add PHP 8.1 classes, constants, functions

--- attic/update.sh
+++ attic/update.sh
@@ -4,10 +4,10 @@ cat - > php.vim
 
 test -e update.out && rm update.out
 
-for version in 5.6 7.0 7.1 7.2 7.3 7.4 8.0; do
+for version in 5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1; do
     php$version 10-collect.php 1>>update.out
 done
 
 cat update.out | \
-    php8.0 20-generate.php | \
-    php8.0 30-update.php php.vim
+    php8.1 20-generate.php | \
+    php8.1 30-update.php php.vim
